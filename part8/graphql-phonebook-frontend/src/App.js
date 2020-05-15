a6afb681f4b4469db0eb2ef08fd1040472aea1f8
Dan <Dan@METABOX>
2020-05-15 08:38:35 -1000

    completed 8.21

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { useQuery, useApolloClient } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   if (result.loading)  {
     return <div>loading...</div>
@@ -22,12 +24,32 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
       <Persons persons = {result.data.allPersons}/>
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
+      <button onClick={logout}>logout</button>
     </div>
   )
 }
